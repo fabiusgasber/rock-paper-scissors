abe4d94fb08dec1b962faae5d45df22a2ee88c59
Fabius Gasber <fabiusgasber@gmail.com>
2023-02-15 14:01:16 +0100

    Add function to compare user and computer selections

--- javascript.js
+++ javascript.js
@@ -13,3 +13,23 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerChoice, computerChoice) {
+    let playerSelection = playerChoice.toLowerCase();
+    let computerSelection = computerChoice.toLowerCase();
+
+    if (playerSelection === "rock" && computerSelection === "rock" || 
+        playerSelection === "paper" && computerSelection === "paper" || 
+        playerSelection === "scissors" && computerSelection === "scissors") {
+            return `Tie! Player selected: ${playerSelection} computer selected: ${computerSelection}`;
+        }
+    
+    else if (playerSelection === "rock" && computerSelection === "scissors" || 
+             playerSelection === "paper" && computerSelection === "rock" || 
+             playerSelection === "scissors" && computerSelection === "paper") {
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+             }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
