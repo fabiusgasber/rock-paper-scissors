639c4cc1dd81ec2a18bf7f9330b4d76e9a02ad38
Fabius Gasber <fabiusgasber@gmail.com>
2023-02-15 13:21:16 +0100

    Add function to make the computer's play

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3);
+
+    if (choice === 0) {
+        return "Rock";
+    }
+
+    else if (choice === 1){
+        return "Paper";
+    }
+
+    else {
+        return "Scissors";
+    }
+}
