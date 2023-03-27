0dfd9882d83b22ea03d18bd891d5bd50fca321fa
Fabius Gasber <fabiusgasber@gmail.com>
2023-03-27 13:21:08 +0200

    Minor js changes

--- javascript.js
+++ javascript.js
@@ -29,15 +29,14 @@ function playRound(e) {
 
 function checkSelections(playerChoice, computerChoice) {
     const result = document.querySelector('#result');
-    if (playerChoice === computerChoice) {
-            result.textContent = `Tie! Player selected: ${playerChoice} Computer selected: ${computerChoice}`;
-        }
-    
-    else if (playerChoice === "rock" && computerChoice === "scissors" || 
+    if (playerChoice === "rock" && computerChoice === "scissors" || 
              playerChoice === "paper" && computerChoice === "rock" || 
              playerChoice === "scissors" && computerChoice === "paper") {
              result.textContent = `You won! Player selected: ${playerChoice} Computer selected: ${computerChoice}`;
              }
+    else if (playerChoice === computerChoice) {
+        result.textContent = `Tie! Player selected: ${playerChoice} Computer selected: ${computerChoice}`;
+    }
     else {
         result.textContent = `You lose! Player selected: ${playerChoice} Computer selected: ${computerChoice}`;
     }
